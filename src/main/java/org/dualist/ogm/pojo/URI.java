ebe783de2ac7b2305e7fb3d1c531bfc0a2698ab4
Marko Koivisto <marko.kooo@gmail.com>
2020-04-04 07:45:01 +0300

    URI equals operation added

--- src/main/java/org/dualist/ogm/pojo/URI.java
+++ src/main/java/org/dualist/ogm/pojo/URI.java
@@ -20,5 +20,13 @@ public class URI {
 		return uri;
 	}
 	
+	  @Override
+	    public boolean equals(Object object)
+	    {
+		  if( ((URI)object).uri.equals(uri)) {
+			   return true;
+		  }
+		  return false;
+	    }
 
 }
